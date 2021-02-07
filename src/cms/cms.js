79d83b3e7be3c13169095962c73986fd71d4af1f
Gabriel Giussi <gagiussi@gmail.com>
2021-02-07 20:09:04 -0300

    First commit

---
+++ src/cms/cms.js
@@ -0,0 +1,12 @@
+import CMS from 'netlify-cms-app'
+//import uploadcare from 'netlify-cms-media-library-uploadcare'
+//import cloudinary from 'netlify-cms-media-library-cloudinary'
+
+//import AboutPagePreview from './preview-templates/AboutPagePreview'
+
+//CMS.registerMediaLibrary(uploadcare)
+//CMS.registerMediaLibrary(cloudinary)
+
+CMS.init()
+
+CMS.registerPreviewTemplate('about', AboutPagePreview)
